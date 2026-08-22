9040bd303042e2cf7ed5c7ebfd2567c688621e1d
iToussaint <iradukundatoussaint01@gmail.com>
2026-08-22 19:05:53 +0200

    feat: add lucide-react dependency and update HomePage component to use FileText icon

--- fullstack/secure-vault/src/features/Home.tsx
+++ fullstack/secure-vault/src/features/Home.tsx
@@ -1,5 +1,8 @@
+import { FileText } from "lucide-react";
+
 function HomePage() {
-  return <h1 className="text-3xl">Home page</h1>;
+  return <h1 className="text-3xl">
+    <FileText></FileText> Home page</h1>;
 }
 
 export default HomePage;
